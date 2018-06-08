041d5ae8677e4b06dade495a8da819382faaac40
Eric Brisco <ebrisco@corumdigital.com>
2018-06-08 13:44:13 -0400

    FIX: build for 0.12

--- src/Concurrent/BoundedQueue.purs
+++ src/Concurrent/BoundedQueue.purs
@@ -14,8 +14,9 @@ module Concurrent.BoundedQueue
 
 import Prelude
 
-import Control.Monad.Aff (Aff)
-import Control.Monad.Aff.AVar as AV
+import Effect.Aff (Aff)
+import Effect.Aff.AVar (AVar)
+import Effect.Aff.AVar as AVar
 import Data.Array (unsafeIndex)
 import Data.Maybe (Maybe(..))
 import Data.Unfoldable (replicateA)
@@ -24,42 +25,42 @@ import Partial.Unsafe (unsafePartial)
 newtype BoundedQueue a =
   BoundedQueue
     { size ∷ Int
-    , contents ∷ Array (AV.AVar a)
-    , readPos ∷ AV.AVar Int
-    , writePos ∷ AV.AVar Int
+    , contents ∷ Array (AVar a)
+    , readPos ∷ AVar Int
+    , writePos ∷ AVar Int
     }
 
 -- | Creates a new `BoundedQueue` with the given capacity,
-new ∷ ∀ a eff. Int → Aff (avar ∷ AV.AVAR | eff) (BoundedQueue a)
+new ∷ ∀ a. Int → Aff (BoundedQueue a)
 new size = do
-  contents ← replicateA size AV.makeEmptyVar
-  readPos ← AV.makeVar 0
-  writePos ← AV.makeVar 0
+  contents ← replicateA size AVar.empty
+  readPos ← AVar.new 0
+  writePos ← AVar.new 0
   pure (BoundedQueue { size, contents, readPos, writePos })
 
 -- | Writes an element to the given queue. Will block if the queue is full until
 -- | someone reads from it.
-write ∷ ∀ a eff. BoundedQueue a → a → Aff (avar ∷ AV.AVAR | eff) Unit
+write ∷ ∀ a. BoundedQueue a → a → Aff Unit
 write (BoundedQueue q) a = do
-  w ← AV.takeVar q.writePos
-  AV.putVar a (unsafePartial unsafeIndex q.contents w)
-  AV.putVar ((w + 1) `mod` q.size) q.writePos
+  w ← AVar.take q.writePos
+  AVar.put a (unsafePartial unsafeIndex q.contents w)
+  AVar.put ((w + 1) `mod` q.size) q.writePos
 
 -- | Reads an element from the given queue, will block if the queue is empty,
 -- | until someone writes to it.
-read ∷ ∀ a eff. BoundedQueue a → Aff (avar ∷ AV.AVAR | eff) a
+read ∷ ∀ a. BoundedQueue a → Aff a
 read (BoundedQueue q) = do
-  r ← AV.takeVar q.readPos
-  v ← AV.takeVar (unsafePartial unsafeIndex q.contents r)
-  AV.putVar ((r + 1) `mod` q.size) q.readPos
+  r ← AVar.take q.readPos
+  v ← AVar.take (unsafePartial unsafeIndex q.contents r)
+  AVar.put ((r + 1) `mod` q.size) q.readPos
   pure v
 
 -- | Checks whether the given queue is empty. Never blocks.
-isEmpty ∷ ∀ a eff. BoundedQueue a → Aff (avar ∷ AV.AVAR | eff) Boolean
+isEmpty ∷ ∀ a. BoundedQueue a → Aff Boolean
 isEmpty (BoundedQueue q) = do
-  AV.tryReadVar q.readPos >>= case _ of
+  AVar.tryRead q.readPos >>= case _ of
     Nothing → pure true
-    Just r → AV.tryReadVar (unsafePartial unsafeIndex q.contents r) <#> case _ of
+    Just r → AVar.tryRead (unsafePartial unsafeIndex q.contents r) <#> case _ of
       Nothing → true
       Just _ → false
 
@@ -68,15 +69,15 @@ isEmpty (BoundedQueue q) = do
 -- |
 -- | *Careful!* If other readers are blocked on the queue `tryRead` will also
 -- | block.
-tryRead ∷ ∀ a eff. BoundedQueue a → Aff (avar ∷ AV.AVAR | eff) (Maybe a)
+tryRead ∷ ∀ a. BoundedQueue a → Aff (Maybe a)
 tryRead (BoundedQueue q) = do
-  r ← AV.takeVar q.readPos
-  AV.tryTakeVar (unsafePartial unsafeIndex q.contents r) >>= case _ of
+  r ← AVar.take q.readPos
+  AVar.tryTake (unsafePartial unsafeIndex q.contents r) >>= case _ of
     Just v → do
-      AV.putVar ((r + 1) `mod` q.size) q.readPos
+      AVar.put ((r + 1) `mod` q.size) q.readPos
       pure (Just v)
     Nothing → do
-      AV.putVar r q.readPos
+      AVar.put r q.readPos
       pure Nothing
 
 -- | Attempts to write an element into the given queue. If the queue is full,
@@ -84,13 +85,13 @@ tryRead (BoundedQueue q) = do
 -- |
 -- | *Careful!* If other writers are blocked on the queue `tryWrite` will also
 -- | block.
-tryWrite ∷ ∀ a eff. BoundedQueue a → a → Aff (avar ∷ AV.AVAR | eff) Boolean
+tryWrite ∷ ∀ a. BoundedQueue a → a → Aff Boolean
 tryWrite (BoundedQueue q) a = do
-  w ← AV.takeVar q.writePos
-  AV.tryPutVar a (unsafePartial unsafeIndex q.contents w) >>= if _
+  w ← AVar.take q.writePos
+  AVar.tryPut a (unsafePartial unsafeIndex q.contents w) >>= if _
     then do
-      AV.putVar ((w + 1) `mod` q.size) q.writePos
+      AVar.put ((w + 1) `mod` q.size) q.writePos
       pure true
     else do
-      AV.putVar w q.writePos
+      AVar.put w q.writePos
       pure false
