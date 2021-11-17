1533517442b458ac5a3c2be39fa109fd6cfacff6
Thomas Honeyman <admin@thomashoneyman.com>
2021-11-16 20:43:53 -0500

    Run purs-tidy

--- src/Concurrent/BoundedQueue/Internal.purs
+++ src/Concurrent/BoundedQueue/Internal.purs
@@ -6,8 +6,8 @@ import Effect.AVar (AVar)
 
 newtype BoundedQueue a =
   BoundedQueue
-    { size     ∷ Int
-    , contents ∷ Array (AVar a)
-    , readPos  ∷ AVar Int
-    , writePos ∷ AVar Int
+    { size :: Int
+    , contents :: Array (AVar a)
+    , readPos :: AVar Int
+    , writePos :: AVar Int
     }
--- src/Concurrent/BoundedQueue/Sync.purs
+++ src/Concurrent/BoundedQueue/Sync.purs
@@ -4,8 +4,7 @@ module Concurrent.BoundedQueue.Sync
   , tryRead
   , tryWrite
   , module Export
-  )
-where
+  ) where
 
 import Prelude
 
@@ -19,51 +18,51 @@ import Effect.AVar as AVarEff
 import Partial.Unsafe (unsafePartial)
 
 -- | Synchronously creates a new `BoundedQueue` with the given capacity.
-new ∷ ∀ a. Int → Effect (BoundedQueue a)
+new :: forall a. Int -> Effect (BoundedQueue a)
 new size = do
-  contents ← replicateA size AVarEff.empty
-  readPos ← AVarEff.new 0
-  writePos ← AVarEff.new 0
+  contents <- replicateA size AVarEff.empty
+  readPos <- AVarEff.new 0
+  writePos <- AVarEff.new 0
   pure (BoundedQueue { size, contents, readPos, writePos })
 
 -- | Synchronously checks whether the given queue is empty. Never blocks.
-isEmpty ∷ ∀ a. BoundedQueue a → Effect Boolean
+isEmpty :: forall a. BoundedQueue a -> Effect Boolean
 isEmpty (BoundedQueue q) = do
   AVarEff.tryRead q.readPos >>= case _ of
-    Nothing → pure true
-    Just r → AVarEff.tryRead (unsafePartial unsafeIndex q.contents r) <#>
+    Nothing -> pure true
+    Just r -> AVarEff.tryRead (unsafePartial unsafeIndex q.contents r) <#>
       case _ of
-        Nothing → true
-        Just _ → false
+        Nothing -> true
+        Just _ -> false
 
 -- | Synchronously attempts to read an element from the given queue. If the
 -- | queue is empty, or there is a concurrent reader, returns `Nothing`.
-tryRead ∷ ∀ a. BoundedQueue a -> Effect (Maybe a)
+tryRead :: forall a. BoundedQueue a -> Effect (Maybe a)
 tryRead (BoundedQueue q) = do
-  mr ← AVarEff.tryTake q.readPos
+  mr <- AVarEff.tryTake q.readPos
   case mr of
-    Just r → do
+    Just r -> do
       AVarEff.tryTake (unsafePartial unsafeIndex q.contents r) >>= case _ of
-        Just v → do
+        Just v -> do
           _ <- AVarEff.tryPut ((r + 1) `mod` q.size) q.readPos
           pure (Just v)
-        Nothing → do
+        Nothing -> do
           _ <- AVarEff.tryPut r q.readPos
           pure Nothing
-    Nothing → pure Nothing
+    Nothing -> pure Nothing
 
 -- | Attempts to write an element into the given queue. If the queue is full,
 -- | or there is a concurrent writer, returns `false` otherwise `true`.
-tryWrite ∷ ∀ a. BoundedQueue a → a → Effect Boolean
+tryWrite :: forall a. BoundedQueue a -> a -> Effect Boolean
 tryWrite (BoundedQueue q) a = do
-  mw ← AVarEff.tryTake q.writePos
+  mw <- AVarEff.tryTake q.writePos
   case mw of
-    Just w → do
-      AVarEff.tryPut a (unsafePartial unsafeIndex q.contents w) >>= if _
-        then do
-          _ ← AVarEff.tryPut ((w + 1) `mod` q.size) q.writePos
+    Just w -> do
+      AVarEff.tryPut a (unsafePartial unsafeIndex q.contents w) >>=
+        if _ then do
+          _ <- AVarEff.tryPut ((w + 1) `mod` q.size) q.writePos
           pure true
         else do
-          _ ← AVarEff.tryPut w q.writePos
+          _ <- AVarEff.tryPut w q.writePos
           pure false
-    Nothing → pure false
+    Nothing -> pure false
